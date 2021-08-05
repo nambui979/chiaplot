4df8ba6874c09594c921e69e1228fa4cde96b7ab
nambui979 <88233892+nambui979@users.noreply.github.com>
2021-08-05 23:05:41 +0700

    Update copy.h

--- include/chia/copy.h
+++ include/chia/copy.h
@@ -4,20 +4,14 @@
  *  Created on: Jun 8, 2021
  *      Author: mad
  */
-
 #ifndef INCLUDE_CHIA_COPY_H_
 #define INCLUDE_CHIA_COPY_H_
-
 #include <chia/settings.h>
-
 #include <string>
 #include <vector>
 #include <stdexcept>
-
 #include <cstdio>
 #include <cstdint>
-
-
 inline
 uint64_t copy_file(const std::string& src_path, const std::string& dst_path)
 {
@@ -30,6 +24,7 @@ uint64_t copy_file(const std::string& src_path, const std::string& dst_path)
 		throw std::runtime_error("fopen() failed");
 	}
 	uint64_t total_bytes = 0;
+	std::vector<uint8_t> buffer(g_read_chunk_size);
 	std::vector<uint8_t> buffer(g_read_chunk_size * 16);
 	while(true) {
 		const auto num_bytes = fread(buffer.data(), 1, buffer.size(), src);
@@ -47,23 +42,19 @@ uint64_t copy_file(const std::string& src_path, const std::string& dst_path)
 	fclose(src);
 	return total_bytes;
 }
-
 inline
 uint64_t final_copy(const std::string& src_path, const std::string& dst_path)
 {
 	if(src_path == dst_path) {
 		return 0;
 	}
-	const std::string tmp_dst_path = dst_path + ".tmp";
-	uint64_t total_bytes = 109521666048ull;
+	const std::string tmp_dst_path = dst_path;
+	uint64_t total_bytes = 0;
 	if(rename(src_path.c_str(), tmp_dst_path.c_str())) {
 		// try manual copy
 		total_bytes = copy_file(src_path, tmp_dst_path);
 	}
 	remove(src_path.c_str());
-	rename(tmp_dst_path.c_str(), dst_path.c_str());
 	return total_bytes;
 }
-
-
 #endif /* INCLUDE_CHIA_COPY_H_ */
